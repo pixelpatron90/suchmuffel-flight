a12365376d9769bd932435868940cbbb81f04334
pixelpatron90 <alxndr.wlfgng.brgmnn@gmail.com>
2025-10-05 22:46:40 +0200

    Last Sync: 2025-10-05 22:46 (Mobile)

--- views/home.php
+++ views/home.php
@@ -5,7 +5,7 @@
 </div>
     <div class="bg-white mt-4 rounded-lg shadow-lg p-6 sm:p-8">
       <form action="https://www.google.com/search" method="get" class="flex flex-col gap-y-3">
-        <input type="search" name="query" placeholder="Wonach suchst du diesmal, Faulpelz?" aria-label="Suchbegriff" required
+        <input id="query" type="search" name="query" placeholder="Wonach suchst du diesmal, Faulpelz?" aria-label="Suchbegriff" required
                class="flex-1 px-4 py-3 border border-gray-200 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400" />
         <button type="submit" class="px-5 py-3 bg-blue-600 text-white rounded-xl font-medium hover:bg-blue-700">
           Zaubersuche!
